3b0bf2cd32bbb2d4d666599572624728882ffd32
bekirbilgec <bekirbilgec42@gmail.com>
2023-06-12 11:27:56 +0300

    create terraform file

--- README.md
+++ README.md
@@ -1,1 +1,54 @@
 # aws-eks-with-terraform
+# Project Name
+
+Project Description and Purpose
+
+This project enables you to create an Amazon Elastic Kubernetes Service (EKS) cluster using [Terraform](https://www.terraform.io/), add IAM Roles for Service Accounts, and configure the EKS Cluster autoscaler.
+
+## Getting Started
+
+Follow these steps to run the project on your local environment.
+
+### Prerequisites
+
+- Install [Terraform](https://www.terraform.io/) on your local machine.
+- Have an [AWS](https://aws.amazon.com/) account and configure AWS CLI.
+- Make sure you have the necessary permissions to use the EKS service in AWS.
+
+### Installation
+
+1. Clone this repository to your local machine:
+
+2. Navigate to the project directory:
+
+3. Edit the `main.tf` file to configure the necessary settings. For example, specify the AWS region, subnet IDs, security group IDs, etc.
+
+4. Initialize the Terraform project:
+
+
+5. View the Terraform plan:
+
+
+6. Apply the changes and create the EKS cluster:
+
+
+7. Verify that the EKS cluster is successfully created and start using it.
+
+## Usage
+
+Once you have created the project, you can start using the created EKS cluster. You can manage and use the EKS cluster using the AWS console or AWS CLI. Depending on the purpose and requirements of your project, you can configure the EKS cluster, deploy applications, and scale as needed.
+
+## Contribution
+
+Pull requests are welcome. For major changes, please open an issue to discuss the changes before submitting a pull request.
+
+## License
+
+This project is licensed under the [MIT License](LICENSE).
+
+---
+
+The above example provides a general guide on how to use the project, configure frontend components, and other related details. Feel free to customize it based on your project's specific needs.
+
+
+
---
+++ terraform/provider.tf
@@ -0,0 +1,156 @@
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+# Create a VPC
+resource "aws_vpc" "k8svpc" {
+  cidr_block = "192.168.0.0/16"
+  tags = {
+    Name = "k8svpc"
+  }
+}
+
+resource "aws_internet_gateway" "k8svpc-igw" {
+  vpc_id = aws_vpc.k8svpc.id
+
+  tags = {
+    Name = "k8svpc-igw"
+  }
+}
+
+
+# private subnet 01
+
+resource "aws_subnet" "private-us-east-1a" {
+  vpc_id            = aws_vpc.k8svpc.id
+  cidr_block        = "192.168.0.0/19"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name                              = "private-us-east-1a"
+    "kubernetes.io/role/internal-elb" = "1"
+    "kubernetes.io/cluster/demo"      = "owned"
+  }
+}
+# private subnet 02
+
+resource "aws_subnet" "private-us-east-1b" {
+  vpc_id            = aws_vpc.k8svpc.id
+  cidr_block        = "192.168.32.0/19"
+  availability_zone = "us-east-1b"
+
+  tags = {
+    Name                              = "private-us-east-1b"
+    "kubernetes.io/role/internal-elb" = "1"
+    "kubernetes.io/cluster/demo"      = "owned"
+  }
+}
+
+# public subnet 01
+
+resource "aws_subnet" "public-us-east-1a" {
+  vpc_id                  = aws_vpc.k8svpc.id
+  cidr_block              = "192.168.64.0/19"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name                         = "public-us-east-1a"
+    "kubernetes.io/role/elb"     = "1" #this instruct the kubernetes to create public load balancer in these subnets
+    "kubernetes.io/cluster/demo" = "owned"
+  }
+}
+# public subnet 02
+
+resource "aws_subnet" "public-us-east-1b" {
+  vpc_id                  = aws_vpc.k8svpc.id
+  cidr_block              = "192.168.96.0/19"
+  availability_zone       = "us-east-1b"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name                         = "public-us-east-1b"
+    "kubernetes.io/role/elb"     = "1" #this instruct the kubernetes to create public load balancer in these subnets
+    "kubernetes.io/cluster/demo" = "owned"
+  }
+}
+
+resource "aws_eip" "nat" {
+  vpc = true
+
+  tags = {
+    Name = "nat"
+  }
+}
+
+resource "aws_nat_gateway" "k8s-nat" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public-us-east-1a.id
+
+  tags = {
+    Name = "k8s-nat"
+  }
+
+  depends_on = [aws_internet_gateway.k8svpc-igw]
+}
+
+# routing table
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.k8svpc.id
+
+  route {
+      cidr_block                 = "0.0.0.0/0"
+      nat_gateway_id             = aws_nat_gateway.k8s-nat.id
+    }
+
+  tags = {
+    Name = "private"
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.k8svpc.id
+
+  route {
+      cidr_block                 = "0.0.0.0/0"
+      gateway_id                 = aws_internet_gateway.k8svpc-igw.id
+    }
+
+  tags = {
+    Name = "public"
+  }
+}
+
+
+# routing table association
+
+resource "aws_route_table_association" "private-us-east-1a" {
+  subnet_id      = aws_subnet.private-us-east-1a.id
+  route_table_id = aws_route_table.private.id
+}
+
+resource "aws_route_table_association" "private-us-east-1b" {
+  subnet_id      = aws_subnet.private-us-east-1b.id
+  route_table_id = aws_route_table.private.id
+}
+
+resource "aws_route_table_association" "public-us-east-1a" {
+  subnet_id      = aws_subnet.public-us-east-1a.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public-us-east-1b" {
+  subnet_id      = aws_subnet.public-us-east-1b.id
+  route_table_id = aws_route_table.public.id
+}
+
